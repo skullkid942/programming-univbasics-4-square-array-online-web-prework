3e55461983d1fd4543ebb37d04980fe7629c82f3
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-29 17:47:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(numbers)
-  numbers = [1,2,3]
   counter = 0 
   
-  while counter < square_array.length do
+  while counter < numbers.length do
+    numbers[counter]**2 
+    counter += 1 
+  end
 end
